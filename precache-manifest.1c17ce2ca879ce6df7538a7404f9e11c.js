c57ec808f539a346a444078a3db2fae7c27f94ae
softskillpro20 <softskill.pro20@gmail.com>
2020-10-11 03:06:10 +0000

    Updates

---
+++ precache-manifest.1c17ce2ca879ce6df7538a7404f9e11c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a89f6cc3a64f238a18ae528915af2cd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7c2455991a1706f0b51d",
+    "url": "/static/css/main.cc7ef2a8.chunk.css"
+  },
+  {
+    "revision": "4438053beda460b18903",
+    "url": "/static/js/2.f7c31fa9.chunk.js"
+  },
+  {
+    "revision": "3e6933cf6bd44f8f91c7aa725d75c668",
+    "url": "/static/js/2.f7c31fa9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c2455991a1706f0b51d",
+    "url": "/static/js/main.06d1c803.chunk.js"
+  },
+  {
+    "revision": "c03bc12de9b8e7c78deb",
+    "url": "/static/js/runtime-main.6adc5e45.js"
+  }
+]);
